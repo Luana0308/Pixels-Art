d8ee78bf781e1b4866da86deb50a288ca75055ad
Luana <luanaff-2008@hotmail.com>
2021-11-23 16:40:11 -0300

    requisito 8 e reescrevendo o requisito 4 fazendo a tabela com javascript

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-window.onload = incialColor()
-
-function incialColor(){
-    document.getElementById("cor1").classList.add("selected")
-}
-
+window.onload = setup
 let PaletColorsList = document.getElementsByClassName("color");
 let black = PaletColorsList[0]
 let pink = PaletColorsList[1]
 let gold = PaletColorsList[2]
 let green = PaletColorsList[3]
-let classSelectedName = "selected" 
+let classSelectedName = "selected"
 
-black.onmousedown = function () {
-    black.classList.add(classSelectedName)
+function setup() {
+    SetInitialColor()
+    clicksConfigure()
+    PrintBoard()
+}
+function SetInitialColor(){
+    document.getElementById("cor1").classList.add("selected")
+}
+function clicksConfigure() {
+    black.onmousedown = function () {
+        removeAllColors()
+        black.classList.add(classSelectedName)
+    };
+    pink.onmousedown = function () {
+        removeAllColors()
+        pink.classList.add(classSelectedName)
+    };
+    gold.onmousedown = function () {
+        removeAllColors()
+        gold.classList.add(classSelectedName)
+    };
+    green.onmousedown = function () {
+        removeAllColors()
+        green.classList.add(classSelectedName)
+    };
+}
+function removeAllColors() {
     pink.classList.remove(classSelectedName)
     gold.classList.remove(classSelectedName)
     green.classList.remove(classSelectedName)
-};
-pink.onmousedown = function () {
-    pink.classList.add(classSelectedName)
     black.classList.remove(classSelectedName)
-    gold.classList.remove(classSelectedName)
-    green.classList.remove(classSelectedName)
-};
-gold.onmousedown = function () {
-    gold.classList.add(classSelectedName)
-    black.classList.remove(classSelectedName)
-    pink.classList.remove(classSelectedName)
-    green.classList.remove(classSelectedName)
-};
-green.onmousedown = function () {
-    green.classList.add(classSelectedName)
-    black.classList.remove(classSelectedName)
-    pink.classList.remove(classSelectedName)
-    gold.classList.remove(classSelectedName)
-};
+}
+function PrintBoard(){
+for (let index = 0; index < 5; index+=1) {
+    let section = document.createElement("section")
+    section.id = "pixel-board"
+    for (let index = 0; index < 5; index+=1) {
+    let butoon = document.createElement("div")
+    butoon.onmousedown = function(){
+        let selectedColor = document.getElementsByClassName("selected")[0]
+        let color = window.getComputedStyle(selectedColor, null).getPropertyValue("background-color")
+        console.log(color)
+        butoon.style.backgroundColor = color
+    }
+    butoon.classList.add("pixel") 
+    section.appendChild(butoon)
+}
+    document.body.appendChild(section)
+}
+}
+
+
+
 
 
