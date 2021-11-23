b80485046d528d280d4f3ff25b4472e4cb7d9027
Luana <luanaff-2008@hotmail.com>
2021-11-23 14:10:44 -0300

    requisito 7 do projeto

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 window.onload = incialColor()
+
 function incialColor(){
-    let firstColor = document.querySelectorAll(".color");
-    firstColor[0].classList.add("selected");
+    document.getElementById("cor1").classList.add("selected")
 }
+
+let PaletColorsList = document.getElementsByClassName("color");
+let black = PaletColorsList[0]
+let pink = PaletColorsList[1]
+let gold = PaletColorsList[2]
+let green = PaletColorsList[3]
+let classSelectedName = "selected" 
+
+black.onmousedown = function () {
+    black.classList.add(classSelectedName)
+    pink.classList.remove(classSelectedName)
+    gold.classList.remove(classSelectedName)
+    green.classList.remove(classSelectedName)
+};
+pink.onmousedown = function () {
+    pink.classList.add(classSelectedName)
+    black.classList.remove(classSelectedName)
+    gold.classList.remove(classSelectedName)
+    green.classList.remove(classSelectedName)
+};
+gold.onmousedown = function () {
+    gold.classList.add(classSelectedName)
+    black.classList.remove(classSelectedName)
+    pink.classList.remove(classSelectedName)
+    green.classList.remove(classSelectedName)
+};
+green.onmousedown = function () {
+    green.classList.add(classSelectedName)
+    black.classList.remove(classSelectedName)
+    pink.classList.remove(classSelectedName)
+    gold.classList.remove(classSelectedName)
+};
+
+
